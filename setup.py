5b94d88778cab6ce3755fca8427bde6dece40203
Yevgnen Koh <wherejoystarts@gmail.com>
2021-01-21 17:42:08 +0800

    Add find_version

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 from setuptools import find_packages, setup
+
+
+def find_version(*paths):
+    with open(os.path.join(os.path.dirname(__file__), *paths)) as f:
+        text = f.read()
+        match = re.search(
+            r'^__version__ = (?P<quote>["\'])(?P<ver>[^"\']+)(?P=quote)', text, re.M
+        )
+        if not match:
+            raise RuntimeError("Unable to find version string.")
+
+        return match.group("ver")
+
+
+VERSION = find_version("felis", "__init__.py")
 
 setup(
     name="felis",
     description="Python toolbox.",
-    version="0.1.0",
+    version=VERSION,
     url="https://github.com/Yevgnen/felis",
     author="Yevgnen Koh",
     author_email="wherejoystarts@gmail.com",
