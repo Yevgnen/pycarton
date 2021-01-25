ec0dd8304fe87711b5d012102870e8a5b79cd456
Yevgnen Koh <wherejoystarts@gmail.com>
2021-01-25 16:33:07 +0800

    Add ManhattanSimilarity

--- felis/pytorch/losses.py
+++ felis/pytorch/losses.py
@@ -4,6 +4,15 @@ import torch
 import torch.nn as nn
 
 
+def manhattan_similarity(v1, v2):
+    return torch.exp(-torch.norm(v1 - v2, p=1, dim=-1, keepdim=True))
+
+
+class ManhattanSimilarity(nn.Module):
+    def forward(self, v1, v2):
+        return manhattan_similarity(v1, v2)
+
+
 class HingeLoss(nn.Module):
     def __init__(self, margin=1.0):
         super().__init__()
